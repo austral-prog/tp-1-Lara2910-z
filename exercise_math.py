c02f239c346e37004ee3033939209fdd86735580
Lara2910-z <lzarantonello@mail.austral.edu.ar>
2025-03-27 23:50:05 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
 def math():
     a = 57
     b = 7
+    #suma
+    print(a + b)
+    #resta
+    print(a - b)
+    #producto
+    print(a*b)
+    #variable c
+    c = a + b
+    #promedio de c
+    print(c/2)
+    #cociente entero
+    print(a//b)
+    #resto de la división entera
+    print(a % b)
+    #valor real de la división
+    print(a/b)
